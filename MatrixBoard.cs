67657e1ea5dec8a91a9d51ec2afc459e2a92e4db
ShySpaceSheep <theminecraftf303@gmail.com>
2023-10-31 23:47:54 +0800

    Added new files

---
+++ MatrixBoard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_Using_WinForms
+{
+    public class MatrixBoard
+    {
+        private MatrixSquare[,] board;
+        private const int BOARD_WIDTH = 22;
+        private const int BOARD_HEIGHT = 12;
+        private int square_width;
+
+        public MatrixBoard()
+        {
+
+        }
+        public void Render()
+        {
+
+        }
+    }
+}
---
+++ MatrixSquare.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_Using_WinForms
+{
+    class MatrixSquare
+    {
+        private Point squareLocation;
+        private bool isOccupied;
+        private bool isPlayable;
+
+        public MatrixSquare(int xLocation, int yLocation)
+        {
+            this.squareLocation.X = xLocation;
+            this.squareLocation.Y = yLocation;
+
+            this.isOccupied = false;
+            this.isPlayable = true;
+        }
+        public Point Location
+        {
+            get { return this.squareLocation; }
+            set { this.squareLocation = value; }
+        }
+        public bool Occupied
+        {
+            get { return this.isOccupied; }
+            set { this.isOccupied = value; }
+        }
+        public bool PLayable
+        {
+            get { return this.isPlayable; }
+            set { this.isPlayable = value; }
+        }
+
+
+    }
+}
