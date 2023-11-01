a4a5de8ec5b00ae46a73ee01716d5a582c03184c
ShySpaceSheep <theminecraftf303@gmail.com>
2023-11-01 23:53:21 +0800

    Updated Tetromino class

--- Tetromino.cs
+++ Tetromino.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Tetris_Using_WinForms
 {
-    public class Tetromino
+    public abstract class Tetromino
     {
+        private int maxWidth;
+        private int maxHeight;
+    }
+    public sealed class IPiece : Tetromino
+    {
+
+    }
+    public sealed class OPiece : Tetromino
+    {
+
+    }
+    public sealed class TPiece : Tetromino
+    {
+
+    }
+    public sealed class SPiece : Tetromino
+    {
+
+    }
+    public sealed class ZPiece : Tetromino
+    {
+
+    }
+    public sealed class JPiece : Tetromino
+    {
+
+    }
+    public sealed class LPiece : Tetromino
+    {
+
     }
 }
