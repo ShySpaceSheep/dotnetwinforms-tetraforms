7d111fa50ab793ad477cbe2cf9ce3dfd1fd7a3b5
ShySpaceSheep <theminecraftf303@gmail.com>
2023-11-02 01:10:17 +0800

    Updated Tetromino.cs

--- Tetromino.cs
+++ Tetromino.cs
@@ -6,10 +6,52 @@ using System.Threading.Tasks;
 
 namespace Tetris_Using_WinForms
 {
+    public class TetrominoGenerator
+    {
+        public Tetromino GenerateTetromino()
+        {
+            switch(Utils.RandomIntRange(1, 8))
+            {
+                case 1:
+                    return new IPiece();
+                case 2:
+                    return new OPiece();
+                case 3:
+                    return new TPiece();
+                case 4:
+                    return new SPiece();
+                case 5:
+                    return new ZPiece();
+                case 6:
+                    return new JPiece();
+                case 7:
+                    return new LPiece();
+                default:
+                    return null;
+            }
+        }
+    }
     public abstract class Tetromino
     {
+        // Per Tetris Wiki definition of each rotation states:
+        enum Rotation
+        {
+            POINT_UP,
+            POINT_LEFT,
+            POINT_DOWN,
+            POINT_RIGHT
+        }
+
         private int maxWidth;
         private int maxHeight;
+        MatrixSquare[,] hitbox;
+
+        public abstract void MoveLeft();
+        public abstract void MoveRight();
+        public abstract void MoveDown();
+        public abstract void Rotate();
+        public abstract void CheckCollisions();
+        public abstract void Place();
     }
     public sealed class IPiece : Tetromino
     {
---
+++ Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_Using_WinForms
+{
+    public static class Utils
+    {
+        public static int RandomIntRange(int min, int max)
+        {
+            return new Random().Next(min, max);
+        }
+    }
+}
